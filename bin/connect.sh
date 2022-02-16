0db40918827a771a783265c5fe48b34ad409c2c3
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2022-02-16 23:22:42 +0900

    skip binary as hex

--- bin/connect.sh
+++ bin/connect.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 WORKDIR=$(pwd)
 cd "$(dirname "$0")"
 
-docker exec -it $(docker-compose ps -q) bash -c "mysql -uroot -proot sakila"
+docker exec -it $(docker-compose ps -q) bash -c "mysql -uroot -proot sakila --skip-binary-as-hex"
